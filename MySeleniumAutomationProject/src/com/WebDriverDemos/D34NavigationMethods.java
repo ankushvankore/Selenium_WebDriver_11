67336974a7ce751c04dee60076231e71daacdc6a
Ankush <ankushvankore@gmail.com>
2025-06-25 10:16:00 +0530

    Date 25-06-2025

---
+++ MySeleniumAutomationProject/src/com/WebDriverDemos/D34NavigationMethods.java
@@ -0,0 +1,28 @@
+package com.WebDriverDemos;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class D34NavigationMethods {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new EdgeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+		//driver.get("https://google.com/");
+		driver.navigate().to("https://google.com/");
+		
+		driver.findElement(By.linkText("About")).click();
+		Thread.sleep(2000);
+		driver.navigate().back();
+		Thread.sleep(2000);
+		driver.navigate().forward();
+		Thread.sleep(2000);
+		driver.navigate().refresh();
+	}
+
+}
